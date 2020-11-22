dbd21930a8f51c580549b7a973e2eb534d349642
KseniaMakara <73828804+KseniaMakara@users.noreply.github.com>
2020-11-22 16:46:27 +0200

    Update L6.2.5.py

--- L6.2.5.py
+++ L6.2.5.py
@@ -2,29 +2,25 @@ import math
 n = int(input("Enter n: "))
 a=[]
 b=[]
-c=[]
 i=1
-e=1
-s=0
+b1=1
+b2=0
+position_in_a = 0
 while i<=n:
     el = ((i - 1) ** 2 / 2 * i ** 2 - 1) + math.factorial(i) * math.sin(i)
     a.append(el)
     i+=1
 print(a)
-for el in a:
-    b=[el for el in a if el<0]
+for i in range(len(a)):
+    if a[i]<0:
+        for i1 in range(i+1):
+            b1*= float(a[i1])
+        b.append(b1)
+        b1 = 1
+    else:
+        for i2 in range(i+1):
+            b2+= (i2+1)*abs(a[i2])
+        b.append(b2)
+        b2=0
 print(b)
-for el in b:
-    e*=el
-print("e ={0}".format(e))
-for el in a:
-    c=[el for el in a if el>=0]
-print(c)
-for el in c:
-    while i<n:
-        k=i
-        el=abs(c[k])*i
-        s = s + el
-        i+=1
-    print("s = {0}".format(s))
 
